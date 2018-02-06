9c47e7cdf83dea1bfb4a1b4386605edfa8b3eecf
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-02-06 12:52:21 -0800

    Determines the biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,17 +1,23 @@
-//Author:
+//Author: Gabriel Caballero
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
-int main()
+int main() 
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  int var1;
+  int var2;
+  int var3
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
-
+cout<<"Please enter a whole number:\n";
+cin>>var1;
+cout<<"Please enter another whole number:\n";
+cin>>var2;
+cout<<"Of those two numbers, the biggest is: ";
+if( var1>=var2) 
+{cout<< var1 << endl;
+}
+cout<<var3<<endl<<"Thank you for playing.\n";
   return 0;
 }
+
